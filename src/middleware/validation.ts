3be57544b8155a023aee7b47261b9364d979330d
Garvit Rai <raigarvit9@gmail.com>
2024-06-28 08:27:45 +0530

    Adding user profile feature and manage restaurant

--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -1,19 +1,51 @@
-import { Request,Response,NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
 import { body, validationResult } from "express-validator";
 
-const handleValidationErrors=async(req:Request,res:Response,next:NextFunction)=>{
-    
-    const errors=validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors:errors.array()});
-    }
-    next();
-}
+const handleValidationErrors = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  next();
+};
 
-export const valdiateMyUserRequest=[
-    body("name").isString().notEmpty().withMessage("Name must be a string"),
-    body("addressLine1").isString().notEmpty().withMessage("Address Line 1 must be a string"),
-    body("city").isString().notEmpty().withMessage("City must be a string"),
-    body("country").isString().notEmpty().withMessage("Country must be a string"),
-    handleValidationErrors,
-]
+export const valdiateMyUserRequest = [
+  body("name").isString().notEmpty().withMessage("Name must be a string"),
+  body("addressLine1")
+    .isString()
+    .notEmpty()
+    .withMessage("Address Line 1 must be a string"),
+  body("city").isString().notEmpty().withMessage("City must be a string"),
+  body("country").isString().notEmpty().withMessage("Country must be a string"),
+  handleValidationErrors,
+];
+
+export const validateMyRestaurantRequest = [
+  body("restaurantName").notEmpty().withMessage("Restaurant Name is Required"),
+  body("city").notEmpty().withMessage("City is Required"),
+  body("country").notEmpty().withMessage("Country is Required"),
+  body("deliveryPrice")
+    .isFloat({ min: 0 })
+    .withMessage("Delivery Price must be a positive number"),
+  body("estimatedDeliveryTime")
+    .isInt()
+    .withMessage("Estimated Delivery time must be positive integer"),
+  body("cuisines")
+    .isArray()
+    .withMessage("Cuisines must be an array")
+    .not()
+    .isEmpty()
+    .withMessage("Cuisines array cannot be empty"),
+  body("menuItems").isArray().withMessage("Menu items must be an array"),
+  body("menuItems.*.name").notEmpty().withMessage("Menu Item name is required"),
+  body("menuItems.*.price")
+    .isFloat({ min: 0 })
+    .notEmpty()
+    .withMessage("Menu Item price is required and must be a positive number"),
+
+  handleValidationErrors,
+];
